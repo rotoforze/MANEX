3a7010797e3da75ecc62b45e74c6392d978d8cd0
EneasMenendez <eneasdmp36@educastur.es>
2026-05-06 10:34:07 +0200

    Arreglos varios

--- core/API/ENV/bbdd.mjs
+++
@@ -1,9 +0,0 @@
-const bbdd= {
-    "HOSTNAME":"127.0.0.1",
-    "USERNAME":"root", 
-    "PASSWORD":"admin",
-    "DATABASE": "dbmanex",
-    "PORT":3309
-}
-
-export default bbdd;
--- ui/ENV/location.js
+++
@@ -1,4 +0,0 @@
-export const endpoint = {
-    "backend": 'http://localhost:80',
-    "backend_login":"http://localhost:80/login"
-}
