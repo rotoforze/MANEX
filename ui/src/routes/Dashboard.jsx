31a602fb7b98442951d2fad9cb88d8965013ad06
EneasMenendez <eneasdmp36@educastur.es>
2026-05-14 12:08:50 +0200

    Dashboard admin casi al completo

--- ui/src/routes/Dashboard.jsx
+++ ui/src/routes/Dashboard.jsx
@@ -1,24 +1,33 @@
-import React from 'react'
-import { useUsers } from '../context/UserContext.jsx'
+import React from 'react';
+import { InfoDashboard } from "../components/Dashboard/InfoDashboard.jsx";
+import '../../public/styles/mainPages.css';
 
 /**
- *
- * Dashboard principal de control de empresa.
- *
- * Este componente actua como pantalla inicial tras el inicio de sesion
- * y muestra informacion general en funcion de los permisos del usuario.
+ * Contenedor principal del Dashboard.
+ * Muestra un resumen general del estado del sistema.
  *
  * @returns {React.JSX.Element}
- * @author Eneas Menendez
- * @version 1.0.0
+ * @author Alex Bernardos Gil
+ * @version 1.1.0
  * @constructor
  */
-export const Dashboard = () => {
-  // const { user } = useUsers()
-
-  return (
-      <div>
-        dashboard
-      </div>
-  )
+export function Dashboard() {
+    return (
+        <div className="d-flex flex-column card w-100 h-100 empleados-container">
+            <div className="d-flex flex-column align-items-start justify-content-center gap-2 w-100 p-4 pb-0">
+                <div className="d-flex flex-column flex-md-row justify-content-between align-items-start w-100 gap-3">
+                    <div>
+                        <h2 className="fw-bold mb-1">Dashboard</h2>
+                        <p className="text-muted mb-0">
+                            Resumen general del estado del sistema.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <hr className="mb-0" />
+            <div className="flex-grow-1 w-100 p-4" style={{ overflowY: 'auto' }}>
+                <InfoDashboard />
+            </div>
+        </div>
+    );
 }
